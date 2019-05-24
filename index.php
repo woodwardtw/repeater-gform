1307ae4f2c2ef47e2546301e7547fab24f11dea7
Tom Woodward <twwoodward@me.com>
2019-05-23 21:26:13 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,90 @@
+<?php 
+/*
+Plugin Name: repeater gform
+Plugin URI:  https://github.com/
+Description: For stuff that's magical
+Version:     1.0
+Author:      ALT Lab
+Author URI:  http://altlab.vcu.edu
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Domain Path: /languages
+Text Domain: my-toolset
+
+*/
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+
+add_action('wp_enqueue_scripts', 'prefix_load_scripts');
+
+function prefix_load_scripts() {                           
+    $deps = array('jquery');
+    $version= '1.0'; 
+    $in_footer = true;
+    wp_enqueue_script('list-js', 'https://cdnjs.cloudflare.com/ajax/libs/list.js/1.5.0/list.min.js', $deps, $version, $in_footer);     
+    wp_enqueue_script('gform-repeater-js', plugin_dir_url( __FILE__) . 'js/gform-repeater.js', $deps, $version, $in_footer); 
+    wp_enqueue_style( 'gform-repeater-main-css', plugin_dir_url( __FILE__) . 'css/gform-repeater-main.css');
+}
+
+// Adjust your form ID
+add_filter( 'gform_form_post_get_meta_7', 'add_my_field' );
+function add_my_field( $form ) {
+ 
+    // Create a Single Line text field for the title
+    $title = GF_Fields::create( array(
+        'type'   => 'text',
+        'id'     => 1002, // The Field ID must be unique on the form
+        'formId' => $form['id'],
+        'label'  => 'Title',
+        'pageNumber'  => 1, // Ensure this is correct
+    ) );
+ 
+    // Create a category for the evidence
+    $description = GF_Fields::create( array(
+        'type'   => 'text',
+        'id'     => 1001, // The Field ID must be unique on the form
+        'formId' => $form['id'],
+        'label'  => 'Category',
+        'pageNumber'  => 1, // Ensure this is correct        
+    ) );
+
+    $year = GF_Fields::create( array(
+        'type'   => 'number',
+        'id'     => 1003, // The Field ID must be unique on the form
+        'formId' => $form['id'],
+        'label'  => 'Year',
+        'pageNumber'  => 1, // Ensure this is correct        
+    ) );
+ 
+    // Create a repeater for the team members and add the name and email fields as the fields to display inside the repeater.
+    $evidence = GF_Fields::create( array(
+        'type'             => 'repeater',
+        //'description'      => 'No max',
+        'id'               => 1000, // The Field ID must be unique on the form
+        'formId'           => $form['id'],
+        'label'            => 'Evidence',
+        'addButtonText'    => 'Add item', // Optional
+        'removeButtonText' => 'Remove item', // Optional
+        //'maxItems'         => 3, // Optional
+        'pageNumber'       => 1, // Ensure this is correct
+        'fields'           => array( $title, $description, $year ), // Add the fields here.
+    ) );
+ 
+    $form['fields'][] = $evidence;
+ 
+    return $form;
+}
+ 
+// Remove the field before the form is saved. Adjust your form ID
+add_filter( 'gform_form_update_meta_7', 'remove_my_field', 10, 3 );
+function remove_my_field( $form_meta, $form_id, $meta_name ) {
+ 
+    if ( $meta_name == 'display_meta' ) {
+        // Remove the Repeater field: ID 1000
+        $form_meta['fields'] = wp_list_filter( $form_meta['fields'], array( 'id' => 1000 ), 'NOT' );
+    }
+ 
+    return $form_meta;
+}
+
+
---
+++ js/gform-repeater.js
@@ -0,0 +1,238 @@
+var items = [
+  {
+    title: 'An authored or edited volume published by an academic book publisher',
+    description: 'writing or publication all'
+  },
+  {
+    title: 'An article in an edited volume published by an academic book publisher',
+    description: 'writing or publication all'
+  },
+  {
+    title: 'An article in a peer-reviewed conference proceeding',
+    description: 'writing or publication all'
+  },
+   {
+    title: 'Peer-reviewed working papers',
+    description: 'writing or publication all'
+  },
+  {
+    title: 'Publication or revision of textbooks',
+    description: 'writing or publication all'
+  },
+   {
+    title: 'Publication of cases',
+    description: 'writing or publication all'
+  },
+   {
+    title: 'Publication of other teaching material',
+    description: 'writing or publication all'
+  },
+   {
+    title: 'Publication in non peer-reviewed journals',
+    description: 'writing or publication all'
+  },
+  {
+    title: 'NGO (non-governmental organization) reports',
+    description: 'writing or publication all'
+  },
+  {
+    title: 'Approved research grant application',
+    description: 'grant or award all'
+  },
+  {
+    title: 'Competitive research awards / prizes',
+    description: 'grant or award all'
+  },
+  {
+    title: 'Competitive teaching awards',
+    description: 'grant or award all'
+  },
+  {
+    title: 'Faculty opponent on dissertation',
+    description: 'dissertation all'
+  },
+  {
+    title: 'Member of dissertation grading committee',
+    description: 'dissertation all'
+  },
+  {
+    title: 'Academic meeting presentations (Conference, Networks, Seminars, Workshops)',
+    description: 'presentation all'
+  },
+  {
+    title: 'Editorship with academic journal',
+    description: 'Editorial all'
+  },
+  {
+    title: 'Service on editorial boards or committees',
+    description: 'Editorial all'
+  },
+  {
+    title: 'Editorship of professional or other business/management publication',
+    description: 'Editorial all'
+  },
+  {
+    title: 'Academic meeting proceedings',
+    description: 'Meeting all'
+  },
+  {
+    title: 'Public inquires',
+    description: 'Service all'
+  },
+  {
+    title: 'PRJ (not on ABS or Noweigan List)',
+    description: 'all'
+  },
+  {
+    title: 'KAU University Press Reports',
+    description: 'Publication all'
+  },
+  {
+    title: 'Validation of scholarly academic status through leadership positions, participation in recognized academic societies and associations, academic fellow status, invited presentations (national / international), peer-review assignments, etc.',
+    description: 'all'
+  },
+  {
+    title: 'PhD courses in the discipline',
+    description: 'Publication all'
+  },
+  {
+    title: 'Service in development projects for collaborations with organizations and other universities',
+    description: 'Service all'
+  },
+  {
+    title: 'Service on boards for teaching, research, and academic development',
+    description: 'Service all'
+  },
+  {
+    title: 'Substantive roles and participation in academic development projects',
+    description: 'Service all'
+  },
+  {
+    title: 'Consulting activities that are material in terms of time and substance',
+    description: 'Service all'
+  },
+  {
+    title: 'Faculty internships',
+    description: 'Service all'
+  },
+  {
+    title: 'Development and presentation of executive education programs',
+    description: 'Service all'
+  },
+  {
+    title: 'Sustained professional work supporting qualified status',
+    description: 'Service all'
+  },
+  {
+    title: 'Significant participation in business professional associations',
+    description: 'Service all'
+  },
+  {
+    title: 'Professional meeting proceedings',
+    description: 'Service all'
+  },
+  {
+    title: 'Professional meeting presentations',
+    description: 'Service all'
+  },
+  {
+    title: 'Relevant, active service on boards of directors',
+    description: 'Service all'
+  },
+  {
+    title: 'Documented continuing professional education experiences',
+    description: 'Education all'
+  },
+  {
+    title: 'Participation in professional events that focus on the practice of business, management, and related issues',
+    description: 'Education all'
+  },
+  {
+    title: 'Participation in other activities that place faculty in direct contact with business or other organizational leaders',
+    description: 'Education all'
+  },
+  {
+    title: 'Visiting & Guest Professor',
+    description: 'Teaching all'
+  },
+  {
+    title: 'Media (TV, Radio, Newspaper Articles & Presentations)',
+    description: 'Teaching all'
+  },
+  {
+    title: 'Development of new courses / programmes',
+    description: 'Teaching all'
+  },
+  {
+    title: 'Teacher Exchange',
+    description: 'Teaching all'
+  },
+  {
+    title: 'Organising Conference, Networks, Seminars, Workshops',
+    description: 'Teaching all'
+  },
+  {
+    title: 'Teaching, Leading, Presenting at Summerschools',
+    description: 'Teaching all'
+  }
+];
+
+
+//watch page
+MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+  var observer = new MutationObserver(function(mutations, observer) {
+      // fired when a mutation occurs
+      console.log(mutations, observer);
+  let theInputs = document.querySelectorAll('input');
+  let theItems = '';
+  items.forEach(function(item){
+     theItems = theItems + '<li onclick="chooseIt(this)">' +item.title + '<span class="extras">' + item.description + '</span></li>';
+  })
+
+  theInputs.forEach(function(input,index){
+    var theId = input.id;
+    var mainId = theId.substring(0, 12);
+    if (mainId === 'input_7_1001' && !document.getElementById('list-'+index)){
+    input.insertAdjacentHTML('afterend','<ul class="tenure-list" id="list-'+index+'" data-field="' + theId + '">'+theItems+'</ul>');
+      input.addEventListener("input", function() {
+      showList('list-'+index, theId);
+    });
+    }
+  })
+
+});
+
+// define what element should be observed by the observer
+// and what types of mutations trigger the callback
+observer.observe(document, {
+  subtree: true,
+  attributes: true
+  //...
+});
+
+
+
+function showList(listId,searchId){
+  let searcher = document.getElementById(searchId)
+  if ( searcher.value.length > 3) {
+    console.log(searcher.value.length)
+    document.getElementById(listId).classList.add('show')
+  } if ( searcher.value.length === 0 ){
+     document.getElementById(listId).classList.remove('show')
+     document.getElementById(searchId).classList.remove('chosen')
+  }
+}
+
+
+function chooseIt(e){
+  let newChoice = e.innerHTML;
+  let fieldId = e.parentNode.dataset.field;
+  document.getElementById(fieldId).value = newChoice;
+  //document.getElementById(fieldId).classList.add('chosen') //problem is this gets copied down 
+  let remove = document.getElementById(e.parentNode.id).classList.remove('show')
+  console.log(document.getElementById(e.parentNode.id.classList))
+  //document.getElementById('the-list').style.height = '0px'
+  //document.getElementById('the-list').style.opacity = '0'
+  document.getElementById('searcher').focus(); //change focus to something else
+}
