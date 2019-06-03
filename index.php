3800a06e14a7b9e8b70b8e617b74e9f78bbe4686
Tom Woodward <twwoodward@me.com>
2019-06-03 10:38:40 -0400

    add acf sync
    sort by year automaticly

--- index.php
+++ index.php
@@ -122,3 +122,83 @@ function all_tenure_records($first_name, $last_name, $array){
     return $html;
 }
 
+
+/*
+FORM TO ACF 
+*/
+
+function update_record(){
+	global $post;
+	$post_id = $post->ID;
+	$row = array(
+		'record_title' => 'my neat title',
+		'record_category' => 'Publication of cases',
+		'record_year' => 2019,
+	);
+	 add_row('faculty_record', $row, $post_id);
+}
+
+
+//add_shortcode( 'update-record', 'update_record' );
+
+
+//SORT ACF YEAR FIELD TO NEWEST ON TOP
+function sort_record_by_year( $value, $post_id, $field ) {
+	// vars
+	$order = array();
+	
+	// bail early if no value
+	if( empty($value) ) {
+		return $value;		
+	}
+	
+	// populate order
+	foreach( $value as $i => $row ) {
+		
+		$order[ $i ] = $row['field_5cf50f064b39c'];
+		
+	}
+	
+	// multisort
+	array_multisort( $order, SORT_DESC, $value );
+	
+	// return	
+	return $value;
+	
+}
+
+add_filter('acf/load_value/name=faculty_record', 'sort_record_by_year', 10, 3);
+
+
+
+
+
+//ACF JSON SAVER
+add_filter('acf/settings/save_json', 'my_acf_json_save_point');
+ 
+function my_acf_json_save_point( $path ) {
+    
+    // update path
+    $path = plugin_dir_path( __FILE__ )  . '/acf-json';
+    // return
+    return $path;
+    
+}
+
+
+add_filter('acf/settings/load_json', 'my_acf_json_load_point');
+
+function my_acf_json_load_point( $paths ) {
+    
+    // remove original path (optional)
+    unset($paths[0]);
+    
+    
+    // append path
+    $paths[] = plugin_dir_path( __FILE__ )  . '/acf-json';
+    
+    
+    // return
+    return $paths;
+    
+}
